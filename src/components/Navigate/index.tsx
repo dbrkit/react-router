582e081400c364e24afdd36568f321ca4f9b7b31
Davide Bruner <davide.bruner@kelyon.com>
2023-08-11 09:34:18 +0200

    feat(router): update Navigate to use replace instead of push

--- src/components/Navigate/index.tsx
+++ src/components/Navigate/index.tsx
@@ -4,9 +4,8 @@ import useRouter from "../../hooks/useRouter";
 
 export default function Navigate({ to }: { to: string }) {
   const router = useRouter();
-  console.log({ router });
   useEffect(() => {
-    router?.push(to);
+    router?.replace(to);
   }, [router, to]);
   return null;
 }
